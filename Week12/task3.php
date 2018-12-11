11c93dfb075aeaa2df9efca00cba9f9d2a30d7b9
alibeklili <38556988+alibeklili@users.noreply.github.com>
2018-12-11 21:42:41 +0600

    Delete task3.php

--- Week12/task3.php
+++
@@ -1,5 +0,0 @@
-<form action='task3_1.php'>
-	Login   <input type="text" name="Login" size="8"/ required /></br>
-	Password<input type="password" size="10" name="Password" required /></br>
-	<input type="submit" value='Click ME'>
-</form>
